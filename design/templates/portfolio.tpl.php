f7c910eb6efdf484768ab74d1e08b88822ada54b
Fred <john.bashh@gmail.com>
2014-09-19 17:00:11 +0200

    ajout url

--- design/templates/portfolio.tpl.php
+++ design/templates/portfolio.tpl.php
@@ -9,26 +9,41 @@ $.getJSON(json_file,function(result){
     $.each(result.references, function(i, field){     
 noeux++;
 $("#sliderContent").append("<li class=\"slide\" id=\"slide-"+field.id+"\"  data-bg=\""+field.bg+"\" >\n\
-    <h2>"+field.titre+"</h2>\n\
+    <h2><a href=\""+field.url+"\" target=\"blank\"> "+field.titre+"</a></h2>\n\
     <div class=\"slideImg\"><img src=\""+chemin_slides+field.img+"\" /></div>\n\
-            </li>");
+                    <div class=\"visitUrl\" id=\"url-"+field.id+"\"><a href=\""+field.url+"\" target=\"blank\">Visiter le site</a></div>\n\
+</li>");
 
 
 $("#sliderBottom").append("<li><div class=\"slideDesc\"><strong>Description: </strong>"+field.description+"</div>\n\
-    <div class=\"slideTech\"><strong>Technique: </strong> "+field.tech+"</div></li>");
+    <div class=\"slideTech\"><strong>Technique: </strong> "+field.tech+"</div></li>\n\
+");
 //
 if (i===0) {  
     $('#bgIntro').css('background','url('+chemin_bg+field.bg+') repeat center center fixed');
         }
 
+
+$( "#slide-"+field.id ).mouseenter(function() {
+    $( "#url-"+field.id ).show();
+});
+
+$( "#slide-"+field.id ).mouseleave(function() {
+    $( "#url-"+field.id ).hide();
+});
+
    });
    
+   
+   
+
 
 
    
 var tailleEcran=$( window ).width();
 var tailleUL=(noeux * tailleEcran) + tailleEcran;
 var moitieEcran = $( window ).height()/2;
+var defTailleEcran=$( window ).width();
 
 $(".slide").css('width',tailleEcran);
 $("#sliderContent").css("width",tailleUL + "px");
@@ -39,12 +54,14 @@ $("#sliderBottom").css("height",moitieEcran + "px");
 $("#sliderBottom li").css('width',tailleEcran + "px");
 $("#sliderBottom li").css("height",moitieEcran + "px")
  
+ $(".slideImg img").css("max-height",(moitieEcran-20) + "px");
+
  
 
-  var tailleEcran=$( window ).width();
-    var defTailleEcran=$( window ).width();
-    var tailleUL=(noeux * tailleEcran) + tailleEcran;
-    var moitieEcran = $( window ).height()/2;
+//  var tailleEcran=$( window ).width();
+//    var defTailleEcran=$( window ).width();
+//    var tailleUL=(noeux * tailleEcran) + tailleEcran;
+//    var moitieEcran = $( window ).height()/2;
     ////////////////////////////////
 
 ///////////////////// RESIZE
@@ -77,11 +94,16 @@ function reposition () {
 
     
     var newPosUL = (tailleEcran*posAct)+(42*(posAct));
-    if (posAct!==0) { $("#sliderContent").css('left',"-"+newPosUL+"px"); }
+    var newPosULbot = (tailleEcran*posAct);
+    if (posAct!==0) { 
+        $("#sliderContent").css('left',"-"+newPosUL+"px");
+        $("#sliderBottom").css('left',"-"+newPosULbot+"px");
+       
+    }
     
-
+$(".slideImg img").css("max-height",(moitieEcran-20) + "px");
     
-}
+} // fin reposition
 
 function calcul() {
     tailleEcran=$( window ).width();
@@ -106,56 +128,78 @@ mooveRight();
 
 
 
-
+//////////////////////////////////////////////////////////////
 function mooveLeft () {
     
       calcul();
       
 
+if ($( "#prevS" ).attr("rel")!=="CLICKED") { 
+    $( "#prevS" ).attr("rel","CLICKED");
+    
     if ( posAct > 0) { 
     posAct--;
     var bgAct=$("#slide-"+(posAct+1)).attr("data-bg");
     var bgActFile=chemin_bg+bgAct;
-    
-    $('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed');
-    $("#sliderContent").animate({left: "+="+(tailleEcran)+"px"},800);
-    $("#sliderBottom").animate({left: "+="+(tailleEcran)+"px"},800);
+      $('#bgIntro').fadeOut();
+    $("#sliderContent").animate({left: "+="+(tailleEcran+42)+"px"},800,function () { 
+                    $('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed'); 
+                    $('#bgIntro').fadeIn();
+                    $( "#prevS" ).attr("rel",""); 
+                });
+    $("#sliderBottom").animate({left: "+="+(tailleEcran+posAct+1)+"px"},800);
     }   
     else if (posAct <= 0 ) {        
             posAct=maxPos; 
-            $("#sliderContent").animate({left: "-"+((noeux * tailleEcran)-(tailleEcran))},800);
-            $("#sliderBottom").animate({left: "-"+((noeux * tailleEcran)-(tailleEcran))},800);
+            $('#bgIntro').fadeOut();
+                    $("#sliderContent").animate({left: "-"+((noeux * tailleEcran)-(tailleEcran-42*(posAct)))},800,function () {
+                        $( "#prevS" ).attr("rel",""); 
+                        $('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed');
+                        $('#bgIntro').fadeIn();
+                    });
+            $("#sliderBottom").animate({left: "-"+((noeux * tailleEcran)-(tailleEcran-5))},800);
             var bgAct=$("#slide-"+(posAct+1)).attr("data-bg");
             var bgActFile=chemin_bg+bgAct;
-        $('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed');
+        
         }
-}
+        }//if pas CLICKED
+} //////////////////////////////////////////////////// fin fonction mooveLeft
 
 function mooveRight () {
     
      calcul();
     
+    if ($( "#nextS" ).attr("rel")!=="CLICKED") {
+            $( "#nextS" ).attr("rel","CLICKED");
 
-if (posAct<maxPos) {
-        posAct++;
-        
-var bgAct=$("#slide-"+(posAct+1)).attr("data-bg");
-var bgActFile=chemin_bg+bgAct;
+            if (posAct<maxPos) {
+                    posAct++;
 
-$('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed');
-
-        
-    $("#sliderContent").animate({left: "-="+(tailleEcran+42)+"px"},800);
-    $("#sliderBottom").animate({left: "-="+(tailleEcran+42)+"px"},800);
-    } else {
-        posAct=0; 
-        $("#sliderContent").animate({left: 0},800);
-        $("#sliderBottom").animate({left: 0},800);
-        var bgAct=$("#slide-"+(posAct+1)).attr("data-bg");
-        var bgActFile=chemin_bg+bgAct;
-        $('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed');
+            var bgAct=$("#slide-"+(posAct+1)).attr("data-bg");
+            var bgActFile=chemin_bg+bgAct;
+            $('#bgIntro').fadeOut();            
+                $("#sliderContent").animate({left: "-="+(tailleEcran+42)+"px"},800,function () { 
+                            $('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed');
+                            $('#bgIntro').fadeIn();
+                            $( "#nextS" ).attr("rel","");
+                        });
+                $("#sliderBottom").animate({left: "-="+(tailleEcran)+"px"},800);
+                } else {
+                    posAct=0; 
+                    $('#bgIntro').fadeOut();
+                     var bgAct=$("#slide-"+(posAct+1)).attr("data-bg");
+                    var bgActFile=chemin_bg+bgAct;
+                            $("#sliderContent").animate({left: 0},800, function () { 
+                                    $('#bgIntro').css('background','url('+bgActFile+') repeat center center fixed'); 
+                                    $('#bgIntro').fadeIn();
+                            $( "#nextS" ).attr("rel","");   
+                            });
+                    $("#sliderBottom").animate({left: 0},800);
+                   
+                         
+                     }
     }
-}
+}/// fin mooveright
 
 
       
